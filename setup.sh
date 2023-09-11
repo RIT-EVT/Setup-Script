13355c5d10ac848da91cb3a8c2241bfaec647df3
ActuallyTaylor <zachary.lineman@gmail.com>
2023-09-10 23:16:14 -0400

    Added commands to explain what is happening.
    Signed-off-by: ActuallyTaylor <zachary.lineman@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This set's up an EVT Development Environment from scratch for a macOS System.
 
+echo "Checking for the existence of Homebrew. If it does not exist, it will be installed"
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
@@ -12,12 +13,14 @@ else
 fi
 
 # Get the brew tap that hosts all of the brew files we need
+echo "Tapping the homebrew tap that contains the ARM Toolchain"
 brew tap actuallytaylor/formulae
 
 # Install the GNU ARM TOOLCHAIN
+echo "Installing the arm toolchain"
 brew install arm-gnu-toolchain@12.rb
 
-
+echo "Adding GCC_ARM_TOOLS_PATH to your shell PATH variables..."
 # Export the environment variable into the PATH based on the SHELL
 if [[ $SHELL = "/bin/zsh" ]]; then
     echo "A warning or error from the next command is expected."
@@ -30,8 +33,10 @@ else
     echo 'Shell $0, not recognized. Please add export GCC_ARM_TOOLS_PATH="/usr/local/bin to your shell config file.'
 fi
 
+echo "Installing CMAKE"
 # Install CMAKE
 brew install --cask cmake
 
+echo "Installing clang-format"
 # Install clang-format
 brew install clang-format
